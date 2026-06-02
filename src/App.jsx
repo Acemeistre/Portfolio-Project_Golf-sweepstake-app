7b4841578bba21c13ebf0221e0a9cea665accfe4
Glenn Niblett <glenn.niblett@hotmail.co.uk>
2026-06-02 21:00:06 +0100

    Add continue button with validation and stage switching logic

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ function App() {
     { id: 1, name: '', colour: '#008BFD', isConfirmed: false },
     { id: 2, name: '', colour: '#F7FF00', isConfirmed: false }
   ]);
+  const [currentStage, setCurrentStage] = useState('selection')
 
   // Fetch player odds from API when page first loads
   useEffect(() => {
@@ -28,10 +29,28 @@ function App() {
     })
   }, [])
   
+  //Check if minimum requirements are met.
+  const confirmedParticipants = participants.filter(p => p.isConfirmed)
+  const isReadyToContinue = selectedTournament !== '' && confirmedParticipants.length >= 2
+
+  // Check for unconfirmed rows with text
+  const hasUnconfirmedText = participants.some(p => !p.isConfirmed && p.name.trim() !== '')
+
+  //Handle continue button click
+  const handleContinue = () => {
+    if (hasUnconfirmedText) {
+      const confirm = window.confirm("You have an unconfirmed participant. Continue without them?")
+    if (!confirm) return
+  }
+  setCurrentStage('draw')
+  }
+
   return (
     <div className="app">
       <Header/>
       <main className="main">
+        {currentStage === 'selection' && (
+        <>
         <TournamentSelector
         selectedTournament={selectedTournament}
         onTournamentChange={setSelectedTournament}
@@ -44,6 +63,31 @@ function App() {
           participants={participants}
           onParticipantsChange={setParticipants}
         />
+        <div className="continue-btn-wrapper">
+          <button 
+            className={`continue-btn ${!isReadyToContinue ? 'continue-btn--active' : 'continue-btn--disabled'}`}
+            onClick={isReadyToContinue ? handleContinue : null}
+            disabled={!isReadyToContinue}
+          >
+            Continue
+          </button>
+        </div>
+        </>
+        )}
+        {currentStage === 'draw' && (
+          <div> 
+            <h2>Stage 2 - The Draw</h2>
+            <button onClick={() => setCurrentStage('selection')}>
+              Back to Selection
+            </button>
+          </div>
+        )}
+        {currentStage === 'scores' && (
+          <div>
+            <h2>Stage 3 - Live Scores</h2>
+            </div>
+        )}
+          
       </main>
     </div>
   )
