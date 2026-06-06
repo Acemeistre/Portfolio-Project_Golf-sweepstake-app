cc65a86b1243600d2a50ae8995b902c3ca19b808
Glenn Niblett <glenn.niblett@hotmail.co.uk>
2026-06-06 13:10:31 +0100

    Added spinner component with react-custom-roulette wheel and draw logic

--- src/components/Stage2_Draw/Spinner/Spinner.jsx
+++ src/components/Stage2_Draw/Spinner/Spinner.jsx
@@ -1,7 +1,61 @@
-import React from 'react'
+// Import useState, wheel from react-custom-roulette and Spinner.css
+import { useState }from 'react'
+import { Wheel } from 'react-custom-roulette'
+import './Spinner.css'
 
-function Spinner() {
-    return <div>Spinner coming soon</div>
+// Define spinner component that receives: currentGroupPlayers, availableParticipants, handleSpin
+function Spinner({currentGroupPlayers, availableParticipants, handleSpin}) {
+    // State: has the draw started yet (boolean)
+    const [isDrawStarted, setIsDrawStarted] = useState(false);
+    // State: should the wheel be spinning (boolean)
+    const [spin, setSpin] = useState(false);
+    // State: which segment is the prize (number)
+    const [prizeNumber, setPrizeNumber] = useState(0);
+
+    // Derived value: format availableParticipants into the data shape the Wheel component expects
+    const drawAvailableParticipants = availableParticipants.map({option: p.name});
+
+    // Handler: what happens when the button is clicked
+    // Set isDrawStarted to true
+    // Pick a random prize number based on available participants length
+    // Set mustSpin to true
+    const handleSpinButton = () => {
+        setIsDrawStarted(true);
+        setPrizeNumber(Math.floor(Math.random() * availableParticipants.length));
+        setSpin(true);
+    }
+    // Handler: what happens when the wheel stops spinning
+    // Set mustSpin to false
+    // Call handleSpin with the participant that was landed on
+    const handleStopSpinning = () => {
+        setSpin(false);
+        handleSpin(availableParticipants[prizeNumber])
+    }
+   // Return the column layout
+
+    // The combined button (Start the Draw? / Good luck!) based on isDrawStarted
+    // The Wheel component with its required props
+    return (
+        <div className="spinner">
+          <button
+            className="spinner__btn"
+            onClick={handleSpinButton}
+            aria-label="Spin wheel"
+            title="Spin wheel"
+            >
+            {isDrawStarted ? 'Good luck!' : 'Start the Draw?'}
+            <br />
+            {isDrawStarted ? 'Spin again' : 'Go!'} 
+            </button>
+            <Wheel 
+            mustStartSpinning={spin}
+            prizeNumber={prizeNumber}
+            data={drawAvailableParticipants}
+            onStopSpinning={handleStopSpinning}
+            />
+
+        </div>
+    )
 }
-
+// Export default
 export default Spinner
