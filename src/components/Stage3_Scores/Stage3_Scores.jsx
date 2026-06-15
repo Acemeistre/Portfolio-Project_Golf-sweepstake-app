95270b80bcd6dfd9d7c20853bf037706c96b3cd8
Glenn Niblett <glenn.niblett@hotmail.co.uk>
2026-06-15 17:00:56 +0100

    Add fetchLeaderboard function with loading and error state to Stage3_Scores

--- src/components/Stage3_Scores/Stage3_Scores.jsx
+++ src/components/Stage3_Scores/Stage3_Scores.jsx
@@ -1,20 +1,32 @@
-import { useEffect } from "react"
+import { useState, useEffect } from "react"
 
 function LiveScores({ drawResults, selectedTournamentData, participants }) {
+    const [leaderboardData, setLeaderboardData] = useState([]);
+    const [isPolling, setIsPolling] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState(null);
+    
+    const fetchLeaderboard = async () => {
+        setIsLoading(true)
+         try {
+        // code that might fail goes here
+            const url = "https://live-golf-data.p.rapidapi.com/leaderboard?orgId=1&tournId=026&year=2025"
+            const response = await fetch(url, {headers: {
+            'x-rapidapi-host': 'live-golf-data.p.rapidapi.com',
+            'x-rapidapi-key': apiKey
+            }})
+            const data = await response.json()
+        setLeaderboardData(data.leaderboardRows)
+            } catch (error) {
+        setError('Something went wrong')
+        }
+        setIsLoading(false)
+    }   
 
     const apiKey = import.meta.env.VITE_GOLF_SLASH_LEADERBOARDS_API_KEY
 
       useEffect(() => {
-        const url = "https://live-golf-data.p.rapidapi.com/leaderboard?orgId=1&tournId=026&year=2025"
-      fetch(url, {
-        headers: {
-            'x-rapidapi-host': 'live-golf-data.p.rapidapi.com',
-            'x-rapidapi-key': apiKey
-            }
-        })
-      .then(response => response.json())
-      .then(data => console.log(data))
-      .catch(error => console.error('API Error:', error))
+        fetchLeaderboard()
   }, [])
 
      return (
